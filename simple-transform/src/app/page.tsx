cadc5e5c0e2c3ef8b05fcf817e0a2391716010c1
Edanriell <Edanriell@gmail.com>
2025-01-12 20:38:16 +0200

    simple-transform

---
+++ simple-transform/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+	// hover bug
+	// return <div className="box" />;
+
+	// hover bug fix
+	return (
+		<div className="box">
+			<div className="box-inner" />
+		</div>
+	);
+}
