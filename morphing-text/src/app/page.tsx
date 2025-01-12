3c1f8131b2e406532fce37a05bc55151216b0406
Edanriell <Edanriell@gmail.com>
2025-01-12 17:54:43 +0200

    morph text helper component

---
+++ morphing-text/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Morph } from "@/app/components/morph";
+
+export default function Home() {
+	return (
+		<>
+			<h1>Test</h1>
+			<Morph>Lauris</Morph>
+			<br />
+			<Morph>Lisa</Morph>
+		</>
+	);
+}
